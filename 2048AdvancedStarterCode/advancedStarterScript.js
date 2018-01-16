00cb1376c062517aefe8855f6bc849c97bca61c8
Jake Soldera <jsoldera@chadwickschool.org>
2018-01-16 08:08:15 -0800

    fix combineup, turn in game

--- 2048AdvancedStarterCode/advancedStarterScript.js
+++ 2048AdvancedStarterCode/advancedStarterScript.js
@@ -1,86 +1,99 @@
-
 //2D array initialized with sample values. To get a blank board initialize all the values to zero
-var board = [[1024,0,0,0],[1024,0,0,0],[128,0,0,0],[0,0,0,0]];
-//var board = [[2,4,8,16],[32,64,128,256],[512,1024,2048,0],[0,0,0,0]];
+var board = [[0,2,0,0],[0,2,0,0],[0,1024,0,0],[0,1024,0,0]];
+
+//start board = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]];
+//lose board = [[2,4,8,16],[32,64,128,256],[2048,1024,8,16],[256,64,0,0]];
+//show all numbers = [[2,4,8,16],[32,64,128,256],[512,1024,2048,0],[0,0,0,0]];
 
 var UP_ARROW = '38';
 var DOWN_ARROW = '40';
 var LEFT_ARROW = '37';
 var RIGHT_ARROW = '39';
-var R = '82';
+var SPACE = '32';
 
 var score = 0
+var lose = " "
+var win = " "
 
 //As soon as webpage loads run these two functions
-$(document).ready(function(){
+$(document).ready(function() {
   addTile();
-	printBoard();
-	console.log("Loaded webpage"); //how you do print statements in javascript
+  printBoard();
+  console.log("Loaded webpage"); //how you do print statements in javascript
 });
 
-function printBoard(){
-  if (isGameOver) {
-    console.log("You lose!")
-  }
-  if (isGameWon) {
-    console.log("You win!")
-  }
-	document.getElementById("score").innerHTML = score;
-	for(var i = 0; i < 4; i++){
-		for(var j = 0; j < 4; j++){
-			var boardID = "r"+i+"c"+j;
-			// if the tile is not zero, put it on the board
-			// else set it to be empty
-			if(board[i][j]!=0){
-				document.getElementById(boardID).innerHTML = board[i][j];
-			}
-			else {
-				document.getElementById(boardID).innerHTML = "";
-			}
-			//Change the different number tiles to different colors
-			switch(board[i][j]){
-				case 2:
-					document.getElementById(boardID).style.background = "#f0e5da";
-					break;//similar to an else if. Makes sure none of the other cases executes if this one does
-				case 4:
-					document.getElementById(boardID).style.background = "#ede2c8";
-					break;
-				case 8:
-					document.getElementById(boardID).style.background = "#feb578";
-					break;
-				case 16:
-					document.getElementById(boardID).style.background = "#ff9962";
-					break;
-				case 32:
-					document.getElementById(boardID).style.background = "#ff8060";
-					break;
-				case 64:
-					document.getElementById(boardID).style.background = "#ff613c";
-					break;
-				case 128:
-					document.getElementById(boardID).style.background = "#efd26d";
-					break;
-				case 256:
-					document.getElementById(boardID).style.background = "#efd15c";
-					break;
-				case 512:
-					document.getElementById(boardID).style.background = "#efcd4a";
-					break;
-				case 1024:
-					document.getElementById(boardID).style.background = "#f0ca36";
-					break;
-				case 2048:
-					document.getElementById(boardID).style.background = "#fedb14";
-					break;
-				default:
-					//similar to the else statement. If none of the other cases execute, this statement will execute
-					// in our game we'll default to the grey background
-					document.getElementById(boardID).style.background = "rgba(238, 228, 218, 0.35)";
-					break;
+function clearBoard() {
+  board = [[0,0,0,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]];
+  addTile();
+}
 
-			}
-		}
-	}
+function printBoard() {
+  if (isGameOver()) {
+    console.log("You lose!")
+    lose = "You lose!"
+  }
+  if (isGameWon()) {
+    console.log("You win!")
+    win = "You win!"
+  }
+  document.getElementById("title").innerHTML = "2048";
+  document.getElementById("score").innerHTML = "Score: " + score;
+  document.getElementById("lose").innerHTML = lose;
+  document.getElementById("win").innerHTML = win;
+  for (var i = 0; i < 4; i++) {
+    for (var j = 0; j < 4; j++) {
+      var boardID = "r" + i + "c" + j;
+      // if the tile is not zero, put it on the board
+      // else set it to be empty
+      if (board[i][j] != 0) {
+        document.getElementById(boardID).innerHTML = board[i][j];
+      } else {
+        document.getElementById(boardID).innerHTML = "";
+      }
+      //Change the different number tiles to different colors
+      switch (board[i][j]) {
+        case 2:
+          document.getElementById(boardID).style.background = "#f0e5da";
+          break; //similar to an else if. Makes sure none of the other cases executes if this one does
+        case 4:
+          document.getElementById(boardID).style.background = "#ede2c8";
+          break;
+        case 8:
+          document.getElementById(boardID).style.background = "#feb578";
+          break;
+        case 16:
+          document.getElementById(boardID).style.background = "#ff9962";
+          break;
+        case 32:
+          document.getElementById(boardID).style.background = "#ff8060";
+          break;
+        case 64:
+          document.getElementById(boardID).style.background = "#ff613c";
+          break;
+        case 128:
+          document.getElementById(boardID).style.background = "#efd26d";
+          break;
+        case 256:
+          document.getElementById(boardID).style.background = "#efd15c";
+          break;
+        case 512:
+          document.getElementById(boardID).style.background = "#efcd4a";
+          break;
+        case 1024:
+          document.getElementById(boardID).style.background = "#f0ca36";
+          break;
+        case 2048:
+          document.getElementById(boardID).style.background = "#fedb14";
+          break;
+        default:
+          //similar to the else statement. If none of the other cases execute, this statement will execute
+          // in our game we'll default to the grey background
+          document.getElementById(boardID).style.background = "rgba(238, 228, 218, 0.35)";
+          break;
+
+      }
+    }
+  }
 }
 
 // function getScore() {
@@ -92,31 +105,33 @@ function printBoard(){
 // }
 
 //show students an ascii conversion tool.
-document.onkeydown = function(e){
-	console.log(e.keyCode);
-	if (e.keyCode == UP_ARROW) {
-		moveTilesUp();
-		combineTilesUp();
-		addTile();
-		console.log("Pressed up");
-	} else if (e.keyCode == DOWN_ARROW) {
-		// down arrow
-		moveTilesDown();
-		combineTilesDown();
-		addTile();
-		console.log("Pressed down");
-	} else if (e.keyCode == RIGHT_ARROW) {
-		moveTilesRight();
-		combineTilesRight();
-		addTile();
-		console.log("Pressed right");
-	} else if (e.keyCode == LEFT_ARROW) {
-		moveTilesLeft();
-		combineTilesLeft();
-		addTile();
-		console.log("Pressed left");
-	}
-	printBoard();
+document.onkeydown = function(e) {
+  console.log(e.keyCode);
+  if (e.keyCode == UP_ARROW) {
+    moveTilesUp();
+    combineTilesUp();
+    addTile();
+    console.log("Pressed up");
+  } else if (e.keyCode == DOWN_ARROW) {
+    // down arrow
+    moveTilesDown();
+    combineTilesDown();
+    addTile();
+    console.log("Pressed down");
+  } else if (e.keyCode == RIGHT_ARROW) {
+    moveTilesRight();
+    combineTilesRight();
+    addTile();
+    console.log("Pressed right");
+  } else if (e.keyCode == LEFT_ARROW) {
+    moveTilesLeft();
+    combineTilesLeft();
+    addTile();
+    console.log("Pressed left");
+  } else if (e.keyCode == SPACE) {
+    clearBoard();
+  };
+  printBoard();
 };
 
 
@@ -152,33 +167,32 @@ function moveTilesUp() {
       while (r !== 0 && board[r][c] !== 0 && board[r - 1][c] === 0) {
         board[r - 1][c] = board[r][c];
         board[r][c] = 0;
-				r--;
+        r--;
       }
     }
   }
- }
+}
 
 function combineTilesUp() {
-  for (var r = board.length - 1; r >= 0; r--) {
-    for (var c = board.length - 1; c >= 0; c--) {
+  for (var r = 0; r < board.length; r++) {
+    for (var c = 0; c < board[r].length; c++) {
       if (r !== 0 && board[r][c] !== 0 && board[r - 1][c] === board[r][c]) {
         board[r - 1][c] = board[r][c] + board[r - 1][c];
         board[r][c] = 0;
         score += board[r][c] + board[r - 1][c]
-				moveTilesUp();
+        moveTilesUp();
       }
     }
   }
 }
 
 function moveTilesDown() {
-
   for (var r = board.length - 1; r >= 0; r--) {
     for (var c = board.length - 1; c >= 0; c--) {
       while (r !== 3 && board[r][c] !== 0 && board[r + 1][c] === 0) {
         board[r + 1][c] = board[r][c];
         board[r][c] = 0;
-				r++;
+        r++;
       }
     }
   }
@@ -191,7 +205,7 @@ function combineTilesDown() {
         board[r + 1][c] = board[r][c] + board[r + 1][c];
         board[r][c] = 0;
         score += board[r][c] + board[r + 1][c]
-				moveTilesDown();
+        moveTilesDown();
       }
     }
   }
@@ -203,7 +217,7 @@ function moveTilesRight() {
       while (c !== 3 && board[r][c] !== 0 && board[r][c + 1] === 0) {
         board[r][c + 1] = board[r][c];
         board[r][c] = 0;
-				c++;
+        c++;
       }
     }
   }
@@ -215,8 +229,8 @@ function combineTilesRight() {
       if (c !== 3 && board[r][c] !== 0 && board[r][c + 1] === board[r][c]) {
         board[r][c + 1] = board[r][c] + board[r][c + 1];
         board[r][c] = 0;
-        score += board[r][c] + board[r][c+1]
-				moveTilesRight();
+        score += board[r][c] + board[r][c + 1]
+        moveTilesRight();
       }
     }
   }
@@ -229,7 +243,7 @@ function moveTilesLeft() {
       while (c !== 0 && board[r][c] !== 0 && board[r][c - 1] === 0) {
         board[r][c - 1] = board[r][c];
         board[r][c] = 0;
-				c--;
+        c--;
       }
     }
   }
@@ -241,8 +255,8 @@ function combineTilesLeft() {
       if (c !== 0 && board[r][c] !== 0 && board[r][c - 1] === board[r][c]) {
         board[r][c - 1] = board[r][c] + board[r][c - 1];
         board[r][c] = 0;
-        score += board[r][c] + board[r][c-1]
-				moveTilesLeft();
+        score += board[r][c] + board[r][c - 1]
+        moveTilesLeft();
       }
     }
   }
@@ -256,7 +270,8 @@ function cantCombineTilesUp() {
       }
     }
   }
-	return true;
+  console.log("Can't combine up");
+  return true;
 }
 
 function cantCombineTilesDown() {
@@ -267,7 +282,8 @@ function cantCombineTilesDown() {
       }
     }
   }
-	return true;
+  console.log("Can't combine down");
+  return true;
 }
 
 function cantCombineTilesRight() {
@@ -278,7 +294,8 @@ function cantCombineTilesRight() {
       }
     }
   }
-	return true;
+  console.log("Can't combine right");
+  return true;
 }
 
 function cantCombineTilesLeft() {
@@ -289,20 +306,35 @@ function cantCombineTilesLeft() {
       }
     }
   }
-	return true;
+  console.log("Can't combine left");
+  return true;
+}
+
+function isBoardFull() {
+  for (var r = 0; r < board.length; r++) {
+    for (var c = 0; c < board[r].length; c++) {
+      if (board[r][c] == 0) {
+        return false;
+      }
+    }
+  }
+  return true;
 }
 
 function isGameWon() {
-  if (board[r][c]=2048) {
-    return true;
+  for (var r = 0; r < board.length; r++) {
+    for (var c = 0; c < board[r].length; c++) {
+      if (board[r][c] == 2048) {
+        return true;
+      }
+    }
   }
-
   return false;
 }
 
 function isGameOver() {
-	if (cantCombineTilesUp && cantCombineTilesDown && cantCombineTilesRight && cantCombineTilesLeft) {
-		return true;
+  if (isBoardFull() && cantCombineTilesUp() && cantCombineTilesDown() && cantCombineTilesRight() && cantCombineTilesLeft()) {
+    return true;
   }
   return false;
 }
